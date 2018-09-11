5611f073ce4190cedbc3740862a5d99217fce33f
unknown <divas.pandey>
2018-09-11 07:58:59 +0530

    Updated WelComeTest.java

--- src/test/java/testpackage/WelcomeTest.java
+++ src/test/java/testpackage/WelcomeTest.java
@@ -4,8 +4,12 @@ public class WelcomeTest {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in hugelearning.com");
-		
+
 		System.out.println("Added few code from sub branch2");
 		System.out.println("Added code by slaveBranch 1");
+		
+		
+		System.out.println("Other code added by sub branch2");
 
+	}
 }
