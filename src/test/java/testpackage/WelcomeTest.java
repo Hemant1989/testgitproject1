5b59a84f49290b66b4ebf74b70efd0c3fb1a6d5a
unknown <divas.pandey>
2018-09-11 07:48:45 +0530

    Updated WelComeTest.java by subbranch2

--- src/test/java/testpackage/WelcomeTest.java
+++ src/test/java/testpackage/WelcomeTest.java
@@ -4,6 +4,8 @@ public class WelcomeTest {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in hugelearning.com");
+		
+		System.out.println("Added few code from sub branch2");
 	}
 
 }
