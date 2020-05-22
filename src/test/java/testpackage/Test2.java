30e5b39554d6a14c3c00ccabc232bc3adef8c992
Hemant1989 <Hemantnegi89@gmail.com>
2020-05-23 00:15:37 +0530

    Updated slave branch code

--- src/test/java/testpackage/Test2.java
+++ src/test/java/testpackage/Test2.java
@@ -1,9 +1,9 @@
 package testpackage;
 
-public class Test1 {
+public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("I am in Test 1 modify");
+		System.out.println("I am in Test 2");
 	}
 
 }
