4e71c02c920a72a7eaa36f539fdadeb30f8592aa
Microsoft <Microsoft@Microsoft-PC>
2020-05-22 23:51:31 +0530

    Updated HelloTest file

--- src/test/java/testpackage/HelloTest.java
+++ src/test/java/testpackage/HelloTest.java
@@ -3,7 +3,7 @@ package testpackage;
 public class HelloTest {
 
 	public static void main(String[] args) {
-		System.out.println("I am in Hello Test");
+		System.out.println("I am in Hello Test modify");
 
 	}
 
