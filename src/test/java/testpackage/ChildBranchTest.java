da5914201f7d9f306cac4db7dc8359d3c415d3f3
Hemant Negi <hemantnegi89@gmail.com>
2020-05-23 23:10:15 +0530

    Update ChildBranchTest.java

--- src/test/java/testpackage/ChildBranchTest.java
+++ src/test/java/testpackage/ChildBranchTest.java
@@ -4,6 +4,8 @@ public class ChildBranchTest {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in child or sub branch");
+				System.out.println("THIS IS END OF PROGRAM");
+
 	}
 
 }
