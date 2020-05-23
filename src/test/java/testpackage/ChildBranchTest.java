83600437268ea7a8a8b52871bfcbd0762e209517
Hemant1989 <Hemantnegi89@gmail.com>
2020-05-23 23:29:40 +0530

    Added code in master

--- src/test/java/testpackage/ChildBranchTest.java
+++ src/test/java/testpackage/ChildBranchTest.java
@@ -5,6 +5,8 @@ public class ChildBranchTest {
 	public static void main(String[] args) {
 		System.out.println("Welcome in child or sub branch");
 				System.out.println("THIS IS END OF PROGRAM");
+				System.out.println("THIS IS END OF PROGRAM12");
+
 
 	}
 
