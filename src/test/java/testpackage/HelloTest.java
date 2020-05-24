d2a17787959f238a43650a64e79e627582ef7561
Hemant1989 <Hemantnegi89@gmail.com>
2020-05-24 23:57:33 +0530

    Added changes

--- src/test/java/testpackage/HelloTest.java
+++ src/test/java/testpackage/HelloTest.java
@@ -5,6 +5,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("I am in Hello Test modify12");
 		System.out.println("updated this file");
+		System.out.println("End of program");
 	}
 
 }
