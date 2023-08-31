dc90e6d4f864ed3241d10b4ab6302c7046cceec5
Hemant Negi <hemantnegi89@gmail.com>
2023-08-31 20:17:58 +0530

    Added  NewTestClass.java file

---
+++ src/test/java/testpackage/NewTestClass.java
@@ -0,0 +1,8 @@
+package testpackage;
+
+public class NewTestClass {
+    public static void main(String[] args) {
+        System.out.print("Newly added class");
+        System.out.print("Updated content");
+    }
+}
